0be6d652136ae63df20879e35a48efad46c7fbeb
Martin Macharia <mosmartin5@gmail.com>
2020-12-30 19:42:42 +0100

    Refactor the user route handler

--- auth/src/routes/current-user.routes.ts
+++ auth/src/routes/current-user.routes.ts
@@ -1,11 +1,10 @@
 import express from 'express';
 
 import { getCurrentUser } from '../controllers/current-user.controller';
-import { auth } from '../middlewares/auth';
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/v1/users/me', currentUser, auth, getCurrentUser);
+router.get('/api/v1/users/me', currentUser, getCurrentUser);
 
 export { router as currentUserRouter };
