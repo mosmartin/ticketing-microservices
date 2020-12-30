50c0d5f64886daadb4154e7d09b7571eb55ae76c
Martin Macharia <mosmartin5@gmail.com>
2020-12-30 12:52:32 +0100

    Format the user signup response

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -19,17 +19,29 @@ interface UserDoc extends Document {
   password: string;
 }
 
-const userSchema: Schema = new Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema: Schema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-});
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
