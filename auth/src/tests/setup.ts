e3b2022820a45d02b1bb3e67f35ddc55b079723e
Martin Macharia <mosmartin5@gmail.com>
2021-01-15 17:55:50 +0100

    Create a global sign-in function
    It will retrieve a sign cookie.

--- auth/src/tests/setup.ts
+++ auth/src/tests/setup.ts
@@ -1,12 +1,21 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 
 beforeAll(async () => {
   process.env.JWT_SECRET_KEY = 'qwerty';
-  
+
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
@@ -28,3 +37,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'qwerty';
+
+  const res = await request(app)
+    .post('/api/v1/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = res.get('Set-Cookie');
+
+  return cookie;
+};
